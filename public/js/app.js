f75429030b06d34fb17fb6545d69382a35f4e7fd
Steve Jamesson <steve@evendev.com>
2015-06-12 09:39:33 -0400

    gmap and home progress

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,34 @@
 $ = jQuery = require('jquery');
 var bootstrap = require('bootstrap');
+var gmaps = require('gmaps');
 
 (function($) {
 
   var App = {};
 
+  App.loadMap = function() {
+    var div = $('#gmap');
+    var title = $(div).data('marker-title');
+    var lng = $(div).data('lng');
+    var lat = $(div).data('lat');
+
+    var map = new gmaps({
+      div: '#gmap',
+      lat: lat,
+      lng: lng,
+      scrollwheel: false,
+      zoom: 10
+    });
+
+    map.addMarker({
+      lat: lat,
+      lng: lng,
+      title: title
+    });
+  };
+
   App.init = function() {
-    console.log('init!');
+    App.loadMap();
   };
 
 
